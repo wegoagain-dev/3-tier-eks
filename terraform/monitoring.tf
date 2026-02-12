1f525157a229eb0efe35fdd948748052379a26a7
wegoagain-dev <tsdevelops00@gmail.com>
2026-02-12 20:06:15 +0000

    updates to terraform

---
+++ terraform/monitoring.tf
@@ -0,0 +1,29 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+  version          = "56.6.2"
+
+  depends_on = [module.eks]
+
+  # Set Grafana admin password
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin123" # In production, use a secure password and consider using Kubernetes secrets to manage sensitive information.
+  }
+
+  # if not service type 'LoadBalancer' would have to use CLI port-forwarding to see the dashboard. command kubectl get svc -n monitoring
+  # if service tpye 'ClusterIP' use kubectl port-forward svc/prometheus-grafana -n monitoring 8080:80
+  set {
+    name  = "grafana.service.type"
+    value = "ClusterIP"
+  }
+
+  # Optional: Persist Grafana dashboards and data
+  set {
+    name  = "grafana.persistence.enabled"
+    value = "false"
+  }
+}
